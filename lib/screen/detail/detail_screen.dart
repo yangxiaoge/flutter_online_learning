29b96e11c94c08e03155d4581611a01f501d69cf
yangjianan <yangjianan@seuic.com>
2020-09-19 11:23:36 +0800

    update

--- lib/screen/detail/detail_screen.dart
+++ lib/screen/detail/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_online_learning/constant.dart';
 import 'package:flutter_online_learning/screen/detail/widget/body.dart';
+import 'package:flutter_online_learning/screen/widget/my_bottom_bar.dart';
 
 class DetailScreen extends StatelessWidget {
   @override
@@ -8,6 +9,7 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
+      bottomNavigationBar: MyBottomBar(),
     );
   }
 
